5a069b6320a05484f8f76ff0ff304b0f7dc36ae1
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2020-12-28 09:41:43 -0300

    Add player class and controls

--- src/pong/Game.java
+++ src/pong/Game.java
@@ -2,17 +2,94 @@ package pong;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
 
-public class Game extends Canvas implements Runnable {
+public class Game extends Canvas implements Runnable, KeyListener {
 
+    private static final long serialVersionUID = 1L;
     public static int WIDTH = 240;
     public static int HEIGHT = 120;
     public static int SCALE = 3;
 
+    public BufferedImage layer = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+    public Player player;
+
     public Game() {
         this.setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
+        this.addKeyListener(this);
+        player = new Player(100, HEIGHT-10);
     }
 
+    public void tick() {
+        player.tick();
+    }
+
+    public void render() {
+        BufferStrategy bs = this.getBufferStrategy();
+
+        if(bs == null) {
+            this.createBufferStrategy(3);
+            return;
+        }
+
+        Graphics g = layer.getGraphics();
+        // Resets player render, avoiding render overflow;
+        g.setColor(Color.black);
+        g.fillRect(0, 0, WIDTH, HEIGHT);
+
+        player.render(g);
+
+        g = bs.getDrawGraphics();
+        g.drawImage(layer, 0, 0, WIDTH*SCALE, HEIGHT*SCALE, null);
+
+        bs.show();
+    }
+
+    @Override
+    public void run() {
+        while(true) {
+            tick();
+            render();
+            try {
+                Thread.sleep(1000/60);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+        // # IMPORTANT:
+        // Treats the event when certain key is preseed.
+        // Keep in mind that this doesn't prevent key released events.
+        // So if there's not a keyReleased event tied with keyPressed events,
+        // you going to have an endless render towards the direction once pressed.
+
+        if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            player.right = true;
+        } else if (e.getKeyCode() ==  KeyEvent.VK_LEFT) {
+            player.left = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            player.right = false;
+        } else if (e.getKeyCode() ==  KeyEvent.VK_LEFT) {
+            player.left = false;
+        }
+    }
 
     public static void main(String[] args) {
         Game game = new Game();
@@ -25,9 +102,9 @@ public class Game extends Canvas implements Runnable {
         frame.pack();
         frame.setVisible(true);
 
-    }
-    @Override
-    public void run() {
+        new Thread(game).start();
 
     }
+
+
 }
